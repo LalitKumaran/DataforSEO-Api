c1a7355dfeedd0e94e68d705df6deb48c550dff3
LalitKumaran <lalitkumaran1602@gmail.com>
2023-09-17 19:39:45 +0530

    final

--- server.js
+++ server.js
@@ -98,77 +98,81 @@ app.get("/:url", async (req, res) => {
       headers,
     })
     .then((page_res) => {
-      console.log("Tasks",page_res.data.tasks);
+      console.log("Tasks", page_res.data.tasks);
       for (const taskList of page_res.data.tasks[0].result) {
-        taskResponse=taskList;
+        taskResponse = taskList;
       }
       console.log("Result:", taskResponse);
     })
     .catch((err) => {
       console.log(err);
     });
-  if (taskResponse.items!==null) {
-    res.json({
-      url: taskResponse.items[0].url,
-      onpage_score: taskResponse.items[0].onpage_score,
-      internal_links_count: taskResponse.items[0].meta.internal_links_count,
-      external_links_count: taskResponse.items[0].meta.external_links_count,
-      images_count: taskResponse.items[0].meta.images_count,
-      images_size: taskResponse.items[0].meta.images_size,
-      scripts_count: taskResponse.items[0].meta.scripts_count,
-      scripts_size: taskResponse.items[0].meta.scripts_size,
-      plain_text_size: taskResponse.items[0].meta.content.plain_text_size,
-      plain_text_rate:
-        taskResponse.items[0].meta.content.plain_text_rate.toFixed(2),
-      plain_text_word_count:
-        taskResponse.items[0].meta.content.plain_text_word_count.toFixed(2),
-      automated_readability_index:
-        taskResponse.items[0].meta.content.automated_readability_index.toFixed(
-          2
-        ),
-      coleman_liau_readability_index:
-        taskResponse.items[0].meta.content.coleman_liau_readability_index.toFixed(
-          2
-        ),
-      dale_chall_readability_index:
-        taskResponse.items[0].meta.content.dale_chall_readability_index.toFixed(
-          2
-        ),
-      flesch_kincaid_readability_index:
-        taskResponse.items[0].meta.content.flesch_kincaid_readability_index.toFixed(
-          2
-        ),
-      smog_readability_index:
-        taskResponse.items[0].meta.content.smog_readability_index.toFixed(2),
-      description_to_content_consistency:
-        taskResponse.items[0].meta.content.description_to_content_consistency.toFixed(
-          2
-        ),
-      title_to_content_consistency:
-        taskResponse.items[0].meta.content.title_to_content_consistency.toFixed(
-          2
-        ),
-      time_to_interactive:
-        taskResponse.items[0].page_timing.time_to_interactive,
-      dom_complete: taskResponse.items[0].page_timing.dom_complete,
-      largest_contentful_paint:
-        taskResponse.items[0].page_timing.largest_contentful_paint,
-      connection_time: taskResponse.items[0].page_timing.connection_time,
-      time_to_secure_connection:
-        taskResponse.items[0].page_timing.time_to_secure_connection,
-      waiting_time: taskResponse.items[0].page_timing.waiting_time,
-      download_time: taskResponse.items[0].page_timing.download_time,
-      duration_time: taskResponse.items[0].page_timing.duration_time,
-      duplicate_title: taskResponse.items[0].duplicate_title,
-      duplicate_description: taskResponse.items[0].duplicate_description,
-      duplicate_content: taskResponse.items[0].duplicate_content,
-      size:
-        taskResponse.items[0].size >=
-        taskResponse.items[0].total_dom_size,
-      cache_control: taskResponse.items[0].cache_control.cachable,
-      seo_friendly_url: taskResponse.items[0].seo_friendly_url,
-    });
-  } else {
+  try {
+    if (taskResponse.items !== null) {
+      res.json({
+        url: taskResponse.items[0].url,
+        onpage_score: taskResponse.items[0].onpage_score,
+        internal_links_count: taskResponse.items[0].meta.internal_links_count,
+        external_links_count: taskResponse.items[0].meta.external_links_count,
+        images_count: taskResponse.items[0].meta.images_count,
+        images_size: taskResponse.items[0].meta.images_size,
+        scripts_count: taskResponse.items[0].meta.scripts_count,
+        scripts_size: taskResponse.items[0].meta.scripts_size,
+        plain_text_size: taskResponse.items[0].meta.content.plain_text_size,
+        plain_text_rate:
+          taskResponse.items[0].meta.content.plain_text_rate.toFixed(2),
+        plain_text_word_count:
+          taskResponse.items[0].meta.content.plain_text_word_count.toFixed(2),
+        automated_readability_index:
+          taskResponse.items[0].meta.content.automated_readability_index.toFixed(
+            2
+          ),
+        coleman_liau_readability_index:
+          taskResponse.items[0].meta.content.coleman_liau_readability_index.toFixed(
+            2
+          ),
+        dale_chall_readability_index:
+          taskResponse.items[0].meta.content.dale_chall_readability_index.toFixed(
+            2
+          ),
+        flesch_kincaid_readability_index:
+          taskResponse.items[0].meta.content.flesch_kincaid_readability_index.toFixed(
+            2
+          ),
+        smog_readability_index:
+          taskResponse.items[0].meta.content.smog_readability_index.toFixed(2),
+        description_to_content_consistency:
+          taskResponse.items[0].meta.content.description_to_content_consistency.toFixed(
+            2
+          ),
+        title_to_content_consistency:
+          taskResponse.items[0].meta.content.title_to_content_consistency.toFixed(
+            2
+          ),
+        time_to_interactive:
+          taskResponse.items[0].page_timing.time_to_interactive,
+        dom_complete: taskResponse.items[0].page_timing.dom_complete,
+        largest_contentful_paint:
+          taskResponse.items[0].page_timing.largest_contentful_paint,
+        connection_time: taskResponse.items[0].page_timing.connection_time,
+        time_to_secure_connection:
+          taskResponse.items[0].page_timing.time_to_secure_connection,
+        waiting_time: taskResponse.items[0].page_timing.waiting_time,
+        download_time: taskResponse.items[0].page_timing.download_time,
+        duration_time: taskResponse.items[0].page_timing.duration_time,
+        duplicate_title: taskResponse.items[0].duplicate_title,
+        duplicate_description: taskResponse.items[0].duplicate_description,
+        duplicate_content: taskResponse.items[0].duplicate_content,
+        size:
+          taskResponse.items[0].size >= taskResponse.items[0].total_dom_size,
+        cache_control: taskResponse.items[0].cache_control.cachable,
+        seo_friendly_url: taskResponse.items[0].seo_friendly_url,
+      });
+    }
+    else{
+      res.json(null);
+    }
+  } catch (err) {
     res.json(null);
   }
 });
